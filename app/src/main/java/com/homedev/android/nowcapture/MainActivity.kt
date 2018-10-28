d89a24322835efe7271976c0d94c31a77bdf653d
Wonpyo Hong <deblan2@naver.com>
2018-10-28 19:45:03 +0900

    Initial commit

---
+++ app/src/main/java/com/homedev/android/nowcapture/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.homedev.android.nowcapture
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
